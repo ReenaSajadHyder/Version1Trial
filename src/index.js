9c933378d7c9c0f89c5489b7f93f40ea80dddb0d
Reena Sajad <reena.sajad@solitontech.com>
2023-08-02 14:49:11 +0530

    add script about prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const index = (a,b) => a - b;
 
 const mathOperations = {
-    sum: function(a,b) {
-        return a
-    },
-    
-    diff: function(a,b) {
-        return a - b;
-    },
-    product: function(a,b) {
-        return a * b
-    }
- }
+   sum: function(a,b) {
+       return a + b;
+   },
+   
+   diff: function(a,b) {
+       return a - b;
+   },
+   product: function(a,b) {
+       return a * b
+   }
+}
 
 
 // export default subtract;
