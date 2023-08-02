bafc341ecfbf52f01751863f9813bdbac5bd3860
Reena Sajad <reena.sajad@solitontech.com>
2023-08-02 11:56:59 +0530

    Make mistakes in index.html and index.js to test the super linter workflow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const index = (a,b) => a - b;
 
+const mathOperations = {
+    sum: function(a,b) {
+        return a
+    },
+    
+    diff: function(a,b) {
+        return a - b;
+    },
+    product: function(a,b) {
+        return a * b
+    }
+ }
+
+
 // export default subtract;
 module.exports = index;
