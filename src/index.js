c45a8947ef0ea36a97671297d0e0e8afa10ccfd4
Reena Sajad <reena.sajad@solitontech.com>
2023-08-03 17:40:50 +0530

    Add .css file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-const index = (a,b) => a - b;
+const index = (a, b) => a - b;
 
 const mathOperations = {
-   sum: function(a,b) {
-       return a + b;
-   },
-   
-   diff: function(a,b) {
-       return a - b;
-   },
-   product: function(a,b) {
-       return a * b
-   }
-}
+  sum: function (a, b) {
+    return a + b;
+  },
 
+  diff: function (a, b) {
+    return a - b;
+  },
+  product: function (a, b) {
+    return a * b;
+  },
+};
 
 // export default subtract;
 module.exports = index;
